02d0c9605f8ee3766b178bf4b3afe0b613b7f536
Cian Mawhinney <cianmawhinney@gmail.com>
2023-06-17 01:36:25 +0000

    Comment out vault instance & add production tag

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,13 +24,17 @@ resource "digitalocean_ssh_key" "personal_key" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDYiNLw++V2Ajl1h8FGvfpGNIOGnuhdunz1mcJ5qZt1U/URZ93BI6coN38p6e6CPEkkUwhkq3cVhi5EfBCh6IUh0Tb8gHZ0fs5FCnMjRGapjFFGMjfGcyRWCOGvPDrOnztSO+SU4wOf18gAoMmdG8fgCiv4sJsGu1Pb7W26VYNySvfVRcvFyXQI9HhvjFvP2xJ0gDNiT0IPYrnS3wHyM5G9RuO5Mmz5AppmlHq19poYm5Tmb6N7Jf1LDAl/73B77YKFRCI3t7z5ojxZdsdbDq14O3dGxWJ95S5PgK0lXFCrgDkwRXKqW6cyAhQ5ccryn6LSMgRSmDqLNEUaOYXFxyLvYXRigXe0VDZGTMNJpIfOqdcGc25TRce5jTrf5OM/m6zBnbtiBcqWyCiNkXKeDxZ1MifUONB+j3zcnWJopLCTl8k3diKghquja6mQMxIaRwjwUnXOQbzSGQWd7jnON0sprK9RTJ47wQbTY9sH3VSFe2YIv0/NwThYiwb0SCDZaZbltiGhFEhVkJeJhtEHANzRc2ojT0LRUNPgXqJdpwBDI7gfkgZ1n9jkMwNZTC7VTNuM6kldWc3yyDnw9XPehyVvt/MBhD7W9U5qOc2Tbq+lD/GhW/7XYC1MI6vt95QLBT5QFkj1mKa2XUGaSMYHiXj5p2wtCTwdSDBpNIm8ZMeb0Q== mawhinnc@tcd.ie"
 }
 
-resource "digitalocean_droplet" "vault" {
-  name = "vault-01"
-  size   = "s-1vcpu-1gb"
-  image  = "rockylinux-9-x64"
-  region = "lon1"
-
-  ssh_keys = [
-    digitalocean_ssh_key.personal_key.fingerprint
-  ]
+resource "digitalocean_tag" "production" {
+  name = "prod"
 }
+
+# resource "digitalocean_droplet" "vault" {
+#   name = "vault-01"
+#   size   = "s-1vcpu-1gb"
+#   image  = "rockylinux-9-x64"
+#   region = "lon1"
+
+#   ssh_keys = [
+#     digitalocean_ssh_key.personal_key.fingerprint
+#   ]
+# }
